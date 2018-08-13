5dafbc9ac740db0ecbc93c548788fc28ee6dd122
Nanduprasad <41770107+Nanduprasad@users.noreply.github.com>
2018-08-13 12:57:35 +0530

    Update ap.py

--- ap.py
+++ ap.py
@@ -1,11 +1,11 @@
-def sumOfAP( a, d,n) :
-    sum = 0
-    i = 0
-    while i < n :
-        sum = sum + a
-        a = a + d
-        i = i + 1
+def sumOfAP( a,d,n):
+    sum=0
+    i=0
+    while i<n :
+        sum=sum+a
+        a=a+d
+        i=i+1
     return sum
-n,a,d = map(int,raw_input().split())
-print (sumOfAP(a, d, n))    
+n,a,d=map(int,raw_input().split())
+print(sumOfAP(a,d,n))    
     
