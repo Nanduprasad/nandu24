e80cd5f5ee0e8a651bfb32804e4fe861b0efc741
Nanduprasad <41770107+Nanduprasad@users.noreply.github.com>
2018-08-14 15:39:56 +0530

    Create minimum.py

---
+++ minimum.py
@@ -0,0 +1,10 @@
+def smallest(arr,n):
+    min = arr[0]
+    for i in range(1, n):
+        if arr[i] < min:
+            min = arr[i]
+    return min
+arr = [10, 324, 45, 90, 9808]
+n = len(arr)
+Ans = smallest(arr,n)
+print (Ans)
