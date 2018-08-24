b56ddd5f7d8319998465a173f990b83665814b2f
Nanduprasad <41770107+Nanduprasad@users.noreply.github.com>
2018-08-24 11:41:29 +0530

    Create numeric.py

---
+++ numeric.py
@@ -0,0 +1,10 @@
+userInput = 0
+while True:
+  try:
+     userInput = int(input("enter something:"))       
+  except ValueError:
+     print("No")
+     continue
+  else:
+     print("Yes")
+     break 
