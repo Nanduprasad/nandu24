3ba3bfa7653da9fc7bb89d633754ca0abeeea17e
Nanduprasad <41770107+Nanduprasad@users.noreply.github.com>
2018-09-05 20:04:04 +0530

    Update prog26.py

--- prog26.py
+++ prog26.py
@@ -1,7 +1,7 @@
 n=raw_input()
-l=[int(n) for n in raw_input().split()]
-for i in range(len(l)):
-    for j in range(i + 1, len(l)):
-        if l[i] > l[j]:
-           l[i], l[j] = l[j], l[i]
-print l
+list=[int(n) for n in raw_input().split()]
+for i in range(len(list)):
+    for j in range(i + 1, len(list)):
+        if list[i] > list[j]:
+           list[i], list[j] = list[j], list[i]
+print list
