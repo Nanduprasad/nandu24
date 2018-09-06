9ba1a08c447244dc55dd01c72c3fbe432000d89e
Nanduprasad <41770107+Nanduprasad@users.noreply.github.com>
2018-09-06 15:43:56 +0530

    Update prog39.py

--- prog39.py
+++ prog39.py
@@ -1,6 +1,6 @@
-a=raw_input().split()
-largest=a[0]
-for large in a:
+alist=[int(n, 10) for n in raw_input().split()]
+largest=alist[0]
+for large in alist:
     if large > largest:
         largest=large
 print(largest)
