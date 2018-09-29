cc78a333619ea01171cff503dd352c86e6e86988
Nanduprasad <41770107+Nanduprasad@users.noreply.github.com>
2018-09-29 15:03:12 +0530

    Create prog48.c

---
+++ prog48.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) 
+{
+	int i,n,array[50],sum=0,avg;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&array[i]);
+	
+	}
+	for(i=0;i<n;i++)
+	{
+		sum=sum+array[i];
+	}
+	avg=sum/n;
+	printf("%d",avg);
+}
