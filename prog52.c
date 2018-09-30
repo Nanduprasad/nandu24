0adf340a178eaae443ce2de582e0f06289e7c50f
Nanduprasad <41770107+Nanduprasad@users.noreply.github.com>
2018-09-30 13:09:41 +0530

    Create prog52.c

---
+++ prog52.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+ int main(void) 
+{
+	int num;
+	scanf("%d",&num);
+	switch(num)
+	{
+		case 1:
+		printf("One");
+		break;
+		case 2:
+		printf("Two");
+		break;
+		case 3:
+		printf("Three");
+		break;
+		case 4:
+		printf("Four");
+		break;
+		case 5:
+		printf("Five");
+		break;
+		case 6:
+		printf("Six");
+		break;
+		case 7:
+		printf("Seven");
+		break;
+		case 8:
+		printf("Eight");
+		break;
+		case 9:
+		printf("Nine");
+		break;
+		case 10:
+		printf("Ten");
+		break;
+		default:
+		printf("enter numbers from 1 to 10");
+	}
+ 
+	return 0;
+}
+ 
